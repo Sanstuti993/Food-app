ebec0a1c691485efb94944be3a8f4f19d3567281
Mitanshi Wadhwani <mitanshiwadhwani@gmail.com>
2024-04-05 12:22:26 +0530

    local

--- app/src/main/java/com/example/prac10foodordering/MixOptionActivity.java
+++ app/src/main/java/com/example/prac10foodordering/MixOptionActivity.java
@@ -26,11 +26,11 @@ public class MixOptionActivity extends AppCompatActivity {
 
         // Define data for each restaurant item
         List<Restaurant> restaurantList = new ArrayList<>();
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 1", "Mix Food 1", "₹100"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 2", "Mix Food 2", "₹150"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 3", "Mix Food 3", "₹200"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 4", "Mix Food 4", "₹250"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 5", "Mix Food 5", "₹300"));
+        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "@String/Restaurant 1", "@String/Mix Food 1", "@String/₹100"));
+        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "@String/Restaurant 2", "@String/Mix Food 2", "@String/₹150"));
+        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "@String/Restaurant 3", "@String/Mix Food 3", "@String/₹200"));
+        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "@String/Restaurant 4", "@String/Mix Food 4", "@String/₹250"));
+        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "@String/Restaurant 5", "@String/Mix Food 5", "@String/₹300"));
 
         // Create RecyclerView adapter
         adapter = new RestaurantAdapter(restaurantList);
