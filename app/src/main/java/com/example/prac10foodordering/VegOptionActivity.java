2c5f1af515a206a37a79e7a47929c37d2012c7eb
sanstuti jain <sanstuti.jain993@gmail.com>
2024-04-07 15:21:27 +0530

    changes

--- app/src/main/java/com/example/prac10foodordering/VegOptionActivity.java
+++ app/src/main/java/com/example/prac10foodordering/VegOptionActivity.java
@@ -24,11 +24,11 @@ public class VegOptionActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         List<Restaurant> restaurantList = new ArrayList<>();
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 1", "Veg Food 1", "₹100"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 2", "Veg Food 2", "₹150"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 3", "Veg Food 3", "₹200"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 4", "Veg Food 4", "₹250"));
-        restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 5", "Veg Food 5", "₹300"));
+        restaurantList.add(new Restaurant(R.drawable.bambayya_pav_bhaji, "Restaurant 1", "pav Bhaji", "₹100"));
+        restaurantList.add(new Restaurant(R.drawable.dosa, "Restaurant 2", "Dosa", "₹150"));
+        restaurantList.add(new Restaurant(R.drawable.cholebhature, "Restaurant 3", "Chole Bhature", "₹200"));
+        restaurantList.add(new Restaurant(R.drawable.chaat, "Restaurant 4", "Chaat corner", "₹250"));
+        //restaurantList.add(new Restaurant(R.drawable.ic_launcher_foreground, "Restaurant 5", "Veg Food 5", "₹300"));
 
         adapter = new RestaurantAdapter(restaurantList);
 
