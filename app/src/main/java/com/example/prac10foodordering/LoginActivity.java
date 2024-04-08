70b78a4a1cbaacbe9c9dbccfcb02f69079a23b63
sanstuti jain <sanstuti.jain993@gmail.com>
2024-04-08 15:23:16 +0530

    blah

--- app/src/main/java/com/example/prac10foodordering/LoginActivity.java
+++ app/src/main/java/com/example/prac10foodordering/LoginActivity.java
@@ -23,42 +23,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Spinner spinner = findViewById(R.id.spinner);
         UsernameEditText = findViewById(R.id.UsernameLogin);
         PasswordEditText = findViewById(R.id.PasswordLogin);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.spinner_items,
-                android.R.layout.simple_spinner_item
-        );
-
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapter);
-        spinner.setSelection(0);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                switch (position) {
-                    case 0: // English
-                        setLocale("en");
-                        break;
-                    case 1: // Spanish
-                        setLocale("es");
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Do nothing
-            }
-        });
         findViewById(R.id.loginButtonLoginPage).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,20 +55,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
         if (!isValidUser) {
-            Toast.makeText(LoginActivity.this, Boolean.toString(isValidUser), Toast.LENGTH_LONG).show();
+            Toast.makeText(LoginActivity.this, "Invalid", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void setLocale(String languageCode) {
-        Locale newLocale = new Locale(languageCode);
-        Locale currentLocale = getResources().getConfiguration().locale;
-
-        if (!currentLocale.equals(newLocale)) {
-            Locale.setDefault(newLocale);
-            Configuration config = new Configuration();
-            config.setLocale(newLocale);
-            getResources().updateConfiguration(config, getResources().getDisplayMetrics());
-            recreate(); // Restart activity to apply language changes
-        }
-    }
 }
--- app/src/main/java/com/example/prac10foodordering/LoginRegisterOption.java
+++ app/src/main/java/com/example/prac10foodordering/LoginRegisterOption.java
@@ -5,13 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView;
 
+import java.util.List;
+import java.util.Locale;
+import android.content.res.Configuration;
 public class LoginRegisterOption extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register_option);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.spinner_items,
+                android.R.layout.simple_spinner_item
+        );
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                switch (position) {
+                    case 0: // English
+                        setLocale("en");
+                        break;
+                    case 1: // Spanish
+                        setLocale("es");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing
+            }
+        });
     }
 
     public void onLoginButtonClick(View view) {
@@ -24,5 +67,17 @@ public class LoginRegisterOption extends AppCompatActivity {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
         finish();
+    }
+    private void setLocale(String languageCode) {
+        Locale newLocale = new Locale(languageCode);
+        Locale currentLocale = getResources().getConfiguration().locale;
+
+        if (!currentLocale.equals(newLocale)) {
+            Locale.setDefault(newLocale);
+            Configuration config = new Configuration();
+            config.setLocale(newLocale);
+            getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+            recreate();
+        }
     }
 }
